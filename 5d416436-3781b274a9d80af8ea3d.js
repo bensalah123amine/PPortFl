a5e6d88765b3abf19b8897bab4eda6114b8e0c22
bensalah123amine <bensalahamine62@gmail.com>
2024-03-17 16:50:29 +0100

    Updates

---
+++ 5d416436-3781b274a9d80af8ea3d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[321],{9042:function(a,t,e){e.d(t,{ZES:function(){return u}});var r=e(3621);function u(a){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z",clipRule:"evenodd"},child:[]}]})(a)}}}]);
+//# sourceMappingURL=5d416436-3781b274a9d80af8ea3d.js.map
